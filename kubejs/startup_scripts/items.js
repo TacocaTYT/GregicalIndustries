1ff52a857c4ce53221ace8c83483dd8a755b2eff
river <laurman.river@gmail.com>
2023-08-18 23:21:30 +0200

    Alpha release

---
+++ kubejs/startup_scripts/items.js
@@ -0,0 +1,7 @@
+StartupEvents.registry('item', e => {
+    //kubejs/assets/kubejs/textures/item/
+    e.create('andesite_bearing').displayName('Andesite Bearing');
+    e.create('pebble').displayName('Pebble');
+    e.create('stone_ball_bearing').displayName('Stone Ball Bearing');
+    e.create('refined_gravel').displayName('Refined Gravel');
+  })
